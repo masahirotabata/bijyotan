3ff389894db4cd665e46faa902427bf073f51d94
masahirotabata <79088468+masahirotabata@users.noreply.github.com>
2025-08-31 22:44:21 +0900

    Add files via upload

---
+++ src/main/java/com/example/demo/config/StaticResourceConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceConfig implements WebMvcConfigurer {
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+      .addResourceHandler("/images/**")
+      .addResourceLocations(
+          "classpath:/static/images/",
+          "classpath:/public/images/",
+          "classpath:/resources/images/"
+      );
+
+    registry
+      .addResourceHandler("/videos/**")
+      .addResourceLocations("classpath:/static/videos/");
+  }
+}
